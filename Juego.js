682d35ae03e69e339ff0e5cbf3b5588a24e3830d
Gary Barzola <gnbarzol@espol.edu.ec>
2019-12-20 15:46:49 -0500

    Doy estilos al juego y creo el archivo js

---
+++ Juego.js
@@ -0,0 +1,8 @@
+var celeste = document.getElementById("celeste");
+var violeta = document.getElementById("violeta");
+var naranja = document.getElementById("naranja");
+var verde = document.getElementById("verde");
+var btnEmpezar = document.getElementById("btnEmpezar");
+
+
+
